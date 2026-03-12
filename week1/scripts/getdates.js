8601ff453dc7148047d41fa0e7aca1c264304226
willlyca31-lab <willlyca31@gmail.com>
2026-03-11 23:19:20 -0600

    add index

---
+++ week1/scripts/getdates.js
@@ -0,0 +1,7 @@
+
+
+
+const year = new Date().getFullYear();
+ document.getElementById("currentyear").textContent = year;
+ document.getElementById("lastModified").textContent = 
+ "Last Modification: "+document.lastModified;
