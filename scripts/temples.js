bd7f9dcc86afdf8940b777c598125662aff264d0
willlyca31-lab <willlyca31@gmail.com>
2026-03-13 13:25:13 -0600

    add temple

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,9 @@
 
+const menuButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
 
+menuButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");});
 
 const year = new Date().getFullYear();
  document.getElementById("currentyear").textContent = year;
